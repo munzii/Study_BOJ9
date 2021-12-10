c1b27f3d6948c406da3e9588c6e7c422cc2363d7
munzii <munzii422@gmail.com>
2021-12-10 22:31:38 +0900

    studying

---
+++ src/P1085.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class P1085 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		int w = Integer.parseInt(st.nextToken());
+		int h = Integer.parseInt(st.nextToken());
+		
+		int xp = Math.min(x, w-x);
+		int yp = Math.min(y, h-y);
+		
+		bw.write(String.valueOf(Math.min(xp, yp)));
+		
+		br.close();
+		bw.flush();
+		bw.close();
+
+	}
+
+}
