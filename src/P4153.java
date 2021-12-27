e8bf03f84bfafa0df5e6631e97487d9ac8be3f1c
munzii <munzii422@gmail.com>
2021-12-27 11:18:24 +0900

    studying

--- src/P4153.java
+++ src/P4153.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class P4153 {
 
@@ -10,6 +11,9 @@ public class P4153 {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
 
 	}
 
