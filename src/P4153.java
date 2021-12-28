60c3ab7acfd91cc3e8bb00e170a231beee4570ee
munzii <munzii422@gmail.com>
2021-12-28 21:13:00 +0900

    studying

--- src/P4153.java
+++ src/P4153.java
@@ -12,8 +12,33 @@ public class P4153 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
-		st = new StringTokenizer(br.readLine());
-		int a = Integer.parseInt(st.nextToken());
+		
+		while(true) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			if(a == 0 && b == 0 && c == 0) break;
+			
+			if((a*a+b*b) == c*c) {
+				bw.write("right"+"\n");
+			}
+			else if(a*a == (b*b+c*c)) {
+				bw.write("right"+"\n");
+			}
+			else if(b*b==(a*a+c*c)) {
+				bw.write("right"+"\n");
+			}
+			else {
+				bw.write("wrong"+"\n");
+			}
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
+		
 
 	}
 
